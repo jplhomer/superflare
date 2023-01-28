722c5706fbaa4666e32d064dfe724dbe9b97ae69
Josh Larson <jplhomer@gmail.com>
2023-01-28 06:45:12 -0600

    Cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,5 @@ module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
   extends: ["custom"],
-  settings: {
-    next: {
-      rootDir: ["apps/*/"],
-    },
-  },
+  settings: {},
 };
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,6 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["turbo", "prettier"],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
   },
 };
