d68f708f6f899def97ea9b7962de18067e5a62bb
Josh Larson <jplhomer@gmail.com>
2023-01-29 09:53:25 -0600

    Add options to migrate

--- examples/tweetmix/app/models/D1Migration.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from 'superflare';
-
-export class D1Migration extends Model {
-  /* superflare-types-start */
-  id!: number;
-  name?: string;
-  applied_at!: string;
-  /* superflare-types-end */
-}
--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -14,15 +14,45 @@ yargs(hideBin(process.argv))
   .command(
     "migrate",
     "⚡️ migrate your database and update types",
-    () => {},
+    (yargs) => {
+      // Option to specify the path to the database
+      yargs.option("db", {
+        alias: "d",
+        describe: "Path to the database",
+
+        // Default to the path in the .wrangler directory
+        default: path.join(
+          process.cwd(),
+          ".wrangler",
+          "state",
+          "d1",
+          "DB.sqlite3"
+        ),
+      });
+
+      // Option to specify the path to the models directory
+      yargs.option("models", {
+        alias: "m",
+        describe: "Path to the models directory",
+
+        // Default to the path in the app directory
+        default: path.join(process.cwd(), "app", "models"),
+      });
+
+      // Option to create a model if it doesn't exist
+      yargs.option("create", {
+        boolean: true,
+        alias: "c",
+        describe: "Create a model if it doesn't exist",
+        default: false,
+      });
+    },
     (argv) => {
-      const db = new Database(
-        path.join(process.cwd(), ".wrangler", "state", "d1", "DB.sqlite3")
-      );
-      const modelsDirectory = path.join(process.cwd(), "app", "models");
+      const db = new Database(argv.db as string);
+      const modelsDirectory = argv.models as string;
       const types = generateTypesFromSqlite(db);
       const results = addTypesToModelsInDirectory(modelsDirectory, types, {
-        createIfNotFound: true,
+        createIfNotFound: argv.create as boolean,
       });
 
       logger.table(results);
