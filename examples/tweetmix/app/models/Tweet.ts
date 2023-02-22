d0ab8df3cca01452ce7df5922cfcc8ef892267cf
Josh Larson <jplhomer@gmail.com>
2023-02-21 18:03:53 -0600

    Clean up

--- examples/tweetmix/app/models/Tweet.ts
+++ examples/tweetmix/app/models/Tweet.ts
@@ -1,8 +1,6 @@
 import { Model } from "superflare";
 
 export class Tweet extends Model {
-  static tableName = "tweets";
-
   /* superflare-types-start */
   id!: number;
   user_id!: number;
@@ -12,20 +10,5 @@ export class Tweet extends Model {
   number_likes!: number;
   number_retweets!: number;
   number_replies!: number;
-
-  attributes!: {
-    id: number;
-    user_id: number;
-    text: string;
-    created_at: string;
-    updated_at: string;
-    number_likes: number;
-    number_retweets: number;
-    number_replies: number;
-  };
   /* superflare-types-end */
-
-  toJSON() {
-    return this.attributes;
-  }
 }
