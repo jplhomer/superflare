0099c1d98a2e1e3846b5ea75059a51ca5a7d6e5d
Josh Larson <jplhomer@gmail.com>
2023-03-04 10:50:40 -0600

    Sort

--- packages/superflare/cli/generate.ts
+++ packages/superflare/cli/generate.ts
@@ -5,17 +5,17 @@ import { migrationHandler, migrationOptions } from "./generate/migration";
 export function generate(yargs: Argv) {
   return yargs
     .command(
-      "migration <name>",
-      "Generate a Migration",
-      // @ts-expect-error: IDK
-      migrationOptions,
-      migrationHandler
-    )
-    .command(
       "job <name>",
       "Generate a Job",
       // @ts-expect-error: IDK
       jobOptions,
       jobHandler
+    )
+    .command(
+      "migration <name>",
+      "Generate a Migration",
+      // @ts-expect-error: IDK
+      migrationOptions,
+      migrationHandler
     );
 }
