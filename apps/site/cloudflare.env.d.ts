5c9e50d3149e582586c1af119d3de06d6df8ba9c
Andrew Patton <andrew@acusti.ca>
2024-07-22 13:28:18 -0700

    Restore ambient workers types + original filenames
    • no need to rename cloudlfare.env.d.ts → env.d.ts
    • i kept the new env.d.ts name for the remix template in order to match the setup of the “official” cloudflare workers remix template (reference: https://github.com/remix-run/remix/tree/main/templates/cloudflare-workers)

--- apps/site/cloudflare.env.d.ts
+++ apps/site/cloudflare.env.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="@cloudflare/workers-types" />
+
 declare module "__STATIC_CONTENT_MANIFEST" {
   const manifest: string;
   export default manifest;
--- examples/remix-cms/cloudflare.env.d.ts
+++ examples/remix-cms/cloudflare.env.d.ts
@@ -4,3 +4,9 @@ declare module "__STATIC_CONTENT_MANIFEST" {
   const manifest: string;
   export default manifest;
 }
+
+declare const process: {
+  env: {
+    NODE_ENV: "development" | "production";
+  };
+};
