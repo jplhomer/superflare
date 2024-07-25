22f47d47c5a423f640868a0e7221c042ee8d636b
Andrew Patton <andrew@acusti.ca>
2024-07-25 14:20:32 -0700

    Refactor getD1Database from wrangler → miniflare@3
    • the CLI migrate command doesn’t work if miniflare is imported statically
    • adding it as a dependency ensures we get the correct types for miniflare v3.x (otherwise we get miniflare v2.x types)
    • the previous version (using wrangler’s getPlatformProxy) was resulting in the following error:
    
    service core:user:__WRANGLER_EXTERNAL_DURABLE_OBJECTS_WORKER: Worker "core:user:__WRANGLER_EXTERNAL_DURABLE_OBJECTS_WORKER"'s binding "Channel" refers to a service "core:user:worker", but no such service is defined.

--- packages/superflare/cli/d1-database.ts
+++ packages/superflare/cli/d1-database.ts
@@ -1,4 +1,5 @@
 import type { D1Database as D1DatabaseType } from "@cloudflare/workers-types";
+import { getWranglerJsonConfig } from "./config";
 
 export async function createD1Database(
   sqliteDbPath: string,
@@ -13,12 +14,38 @@ export async function createD1Database(
   return db as any as D1DatabaseType;
 }
 
-export async function getD1Database(dbName: string, logger = console.log) {
+type DBConfig = {
+  binding: string;
+  database_id: string;
+};
+
+export async function getD1Database(
+  dbName: string,
+  logger = console.log
+): Promise<D1DatabaseType | null> {
+  const wranglerJsonConfig = await getWranglerJsonConfig(process.cwd());
+  if (!wranglerJsonConfig) {
+    logger("Unable to load wrangler.json");
+    return null;
+  }
+
+  const databaseId = wranglerJsonConfig.d1_databases?.find(
+    ({ binding }: DBConfig) => binding === dbName
+  )?.database_id;
+
+  if (!databaseId) {
+    logger(`No d1_databases {"binding": "${dbName}"} item in wrangler.json`);
+    return null;
+  }
+
   const { npxImport } = await import("npx-import");
-  const { getPlatformProxy } = await npxImport<typeof import("wrangler")>(
-    "wrangler",
+  const { Miniflare } = await npxImport<typeof import("miniflare")>(
+    "miniflare",
     logger
   );
-  const { env } = await getPlatformProxy({ experimentalJsonConfig: true });
-  return env[dbName] as D1DatabaseType | undefined;
+
+  const d1Databases = { [dbName]: databaseId };
+  const mf = new Miniflare({ d1Databases, modules: true, script: "" });
+
+  return await mf.getD1Database(dbName);
 }
