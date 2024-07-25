33fe1fe22f0f9b978f45892e95f1f387d2550ff6
Andrew Patton <andrew@acusti.ca>
2024-07-25 09:02:05 -0700

    Replace better-sqlite3 → @miniflare/d1 in tests
    this means no more direct dependency on better-sqlite3

--- packages/superflare/tests/db.ts
+++ packages/superflare/tests/db.ts
@@ -1,13 +1,13 @@
 import type { D1Database as D1DatabaseType } from "@cloudflare/workers-types";
 import { D1Database, D1DatabaseAPI } from "@miniflare/d1";
-import Database from "better-sqlite3";
+import { createSQLiteDB } from "@miniflare/shared";
 
 /**
  * This should only be exported locally for tests, not in places that are used in the package,
  * because it depends on an installed version of `better-sqlite3`.nvm
  */
 export async function createTestDatabase(sql: string) {
-  const sqliteDb = new Database(":memory:");
+  const sqliteDb = await createSQLiteDB(":memory:");
 
   /**
    * Migrating at this step because D1 doesn't allow multi-statement execs.
