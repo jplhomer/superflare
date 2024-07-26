f7359b2e6e5b93ab56c0535aa1cf0910555e07f3
Andrew Patton <andrew@acusti.ca>
2024-07-26 11:37:05 -0700

    Use remix vite:dev for superflare dev command
    preserved the “Using (D1|R2) bindings: …” console logging and updated it to always print (not just in pages mode), because the remix vite:dev command doesn’t provide any of that info to the user

--- packages/superflare/cli/dev.ts
+++ packages/superflare/cli/dev.ts
@@ -48,36 +48,27 @@ export async function devHandler(
   const isWorkersMode = !isPagesMode;
 
   logger.info(
-    `Starting "wrangler" in ${isPagesMode ? "pages" : "workers"} mode...`
+    `Starting dev server in ${isPagesMode ? "pages" : "workers"} mode...`
   );
 
   const config = await getSuperflareConfig(process.cwd(), logger);
   if (!config) {
     logger.warn(
       "Warning: Did not find a `superflare.config.ts` in your project. " +
-        "You will want to add one in order to specify D1 and R2 bindings for Superflare to use."
+        "You will want to add one in order to provide your appKey and D1 and R2 bindings for Superflare to use.\n" +
+        "More info: https://superflare.dev/reference/superflare-config"
     );
   }
 
   const d1Bindings = config?.d1;
 
-  if (
-    isPagesMode &&
-    d1Bindings &&
-    Array.isArray(d1Bindings) &&
-    d1Bindings.length
-  ) {
+  if (d1Bindings && Array.isArray(d1Bindings) && d1Bindings.length) {
     logger.info(`Using D1 binding: ${d1Bindings.join(", ")}`);
   }
 
   const r2Bindings = config?.r2;
 
-  if (
-    isPagesMode &&
-    r2Bindings &&
-    Array.isArray(r2Bindings) &&
-    r2Bindings.length
-  ) {
+  if (r2Bindings && Array.isArray(r2Bindings) && r2Bindings.length) {
     logger.info(`Using R2 bindings: ${r2Bindings.join(", ")}`);
   }
 
@@ -91,52 +82,9 @@ export async function devHandler(
     process.exit(1);
   }
 
-  /**
-   * Normalize bindings. :sigh:
-   * - `workers pages dev` expects bindings in the shape of `binding KEY=VALUE`
-   * - `wrangler dev` expects bindings in the shape of `--var KEY:VALUE`
-   */
-  const normalizedBindings = argv.binding.map((binding) => {
-    if (typeof binding !== "string") return binding;
-
-    const [key, value] = binding.split("=");
-    return isWorkersMode ? `${key}:${value}` : `${key}=${value}`;
-  });
-
-  const args = [
-    "wrangler",
-    isPagesMode && "pages",
-    "dev",
-    isPagesMode && "public",
-    isWorkersMode && workersEntrypoint,
-    isWorkersMode && "--site public",
-    "--compatibility-date",
-    argv.compatibilityDate,
-    "---compatibility-flag",
-    "nodejs_compat",
-    "--port",
-    argv.port,
-    isPagesMode &&
-      d1Bindings?.length &&
-      d1Bindings.map((d1Binding) => `--d1 ${d1Binding}`).join(" "),
-    isPagesMode &&
-      r2Bindings?.length &&
-      r2Bindings.map((r2Binding) => `--r2 ${r2Binding}`).join(" "),
-    ...normalizedBindings.map(
-      (binding) => "--" + `${isPagesMode ? "binding" : "var"} ${binding}`
-    ),
-    "--experimental-json-config",
-    "--test-scheduled",
-    argv.liveReload && "--live-reload",
-  ].filter(Boolean) as string[];
-
-  spawn("npx", args, {
+  spawn("npx", ["remix", "vite:dev"], {
     stdio: "inherit",
     shell: true,
-    env: {
-      ...process.env,
-      // TODO: Remove this when D1 is stable
-      NO_D1_WARNING: "true",
-    },
+    env: process.env,
   });
 }
