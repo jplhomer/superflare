6f504e91babd9a41a87ae027943997be257197aa
Andrew Patton <andrew@acusti.ca>
2024-08-15 00:03:49 -0700

    Move superflareDevProxyVitePlugin → remix-dev pkg
    added new @superflare/remix-dev package to host the superflareDevProxyVitePlugin in such a way that it can be independently imported into users’ vite.config.ts file without then getting included in the built cloudflare workers bundle

---
+++ packages/superflare-remix-dev/README.md
@@ -0,0 +1,9 @@
+# @superflare/remix-dev
+
+This package provides `superflareDevProxyVitePlugin`, a drop-in replacement for remix’s [`cloudflareDevProxyVitePlugin`](https://remix.run/docs/en/main/guides/vite#cloudflare-proxy) that adds support for the superflare feature set to Vite’s dev server.
+
+[Read the docs](https://superflare.dev), or get started now:
+
+```bash
+npx superflare@latest new
+```
---
+++ packages/superflare-remix-dev/index.ts
@@ -0,0 +1,71 @@
+import { type Request } from "@cloudflare/workers-types";
+import { cloudflareDevProxyVitePlugin } from "@remix-run/dev";
+import { type Cloudflare, getLoadContext } from "@superflare/remix";
+import { type Plugin, type ViteDevServer } from "vite";
+import { type GetPlatformProxyOptions } from "wrangler";
+
+/**
+ * This is copied from the workers-sdk repo (used for wrangler’s getPlatformProxy).
+ * Using `waitUntil` means invoking the async function, so a no-op works in dev.
+ * @see https://github.com/cloudflare/workers-sdk/blob/main/packages/wrangler/src/api/integrations/platform/executionContext.ts
+ */
+class ExecutionContext {
+  waitUntil(promise: Promise<any>): void {
+    if (!(this instanceof ExecutionContext)) {
+      throw new Error("Illegal invocation");
+    }
+  }
+  passThroughOnException(): void {
+    if (!(this instanceof ExecutionContext)) {
+      throw new Error("Illegal invocation");
+    }
+  }
+}
+
+export function superflareDevProxyVitePlugin<Env extends { APP_KEY: string }>(
+  options: GetPlatformProxyOptions = {}
+): Plugin {
+  const ctx = new ExecutionContext();
+  let server: ViteDevServer;
+
+  const getLoadContextWrapper = async ({
+    request,
+    context,
+  }: {
+    request: Request;
+    context: { cloudflare: Cloudflare<Env> };
+  }) => {
+    // Use vite dev server to import singleton-dependent modules,
+    // ensuring the Config singleton class is correct and consistent
+    const superflare = (await server.ssrLoadModule("superflare")) as any;
+    const config = (await server.ssrLoadModule("./superflare.config")).default;
+
+    const loadContext = await getLoadContext<Env>({
+      request,
+      context,
+      SuperflareAuth: superflare.SuperflareAuth,
+      SuperflareSession: superflare.SuperflareSession,
+    });
+
+    // Initialize config here (instead of in superflare#handleFetch)
+    config({ request, ctx, env: loadContext.cloudflare.env });
+    return loadContext;
+  };
+
+  const remixVitePlugin = cloudflareDevProxyVitePlugin({
+    ...options,
+    experimentalJsonConfig: true,
+    // @cloudflare/workers-types’ Request type incompatible with global used here:
+    // https://github.com/remix-run/remix/blob/main/packages/remix-dev/vite/cloudflare-proxy-plugin.ts
+    getLoadContext: getLoadContextWrapper as any,
+  });
+
+  return {
+    ...remixVitePlugin,
+    configureServer: async (viteDevServer: ViteDevServer) => {
+      server = viteDevServer;
+      // Hand off middleware installation to cloudflareDevProxyVitePlugin
+      return (remixVitePlugin.configureServer as any)(viteDevServer);
+    },
+  };
+}
