4cfcc568ff2c221d062b59bef7d4bbcf4f80d4ce
Andrew Patton <andrew@acusti.ca>
2024-11-12 17:03:27 +0000

    :shower: Strip down entry.server.tsx files

--- examples/remix-cms/app/entry.server.tsx
+++ examples/remix-cms/app/entry.server.tsx
@@ -1,33 +1,23 @@
-/**
- * By default, Remix will handle generating the HTTP Response for you.
- * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
- * For more information, see https://remix.run/file-conventions/entry.server
- */
-
-import type { AppLoadContext, EntryContext } from "@remix-run/cloudflare";
+import { renderToReadableStream } from "react-dom/server";
+import { type EntryContext } from "@remix-run/cloudflare";
 import { RemixServer } from "@remix-run/react";
 import { isbot } from "isbot";
-import { renderToReadableStream } from "react-dom/server";
 
 export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext,
-  // This is ignored so we can keep it in the template for visibility.  Feel
-  // free to delete this parameter in your app if you're not using it!
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  context: AppLoadContext
+  remixContext: EntryContext
 ) {
   const body = await renderToReadableStream(
     <RemixServer context={remixContext} url={request.url} />,
     {
-      signal: request.signal,
       onError(error: unknown) {
+        responseStatusCode = 500;
         // Log streaming rendering errors from inside the shell
         console.error(error);
-        responseStatusCode = 500;
       },
+      signal: request.signal,
     }
   );
 
@@ -38,7 +28,7 @@ export default async function handleRequest(
   responseHeaders.set("Content-Type", "text/html");
 
   return new Response(body, {
-    headers: responseHeaders,
     status: responseStatusCode,
+    headers: responseHeaders,
   });
 }
