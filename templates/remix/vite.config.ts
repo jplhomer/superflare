2ee914c3440994609b7dcd5987555ebf816bbc33
Andrew Patton <andrew@acusti.ca>
2024-11-15 15:04:24 -0800

    Remove build.minify: true, add v3_lazyRouteDiscovery
    • build: { minify: true } causes variable names to be mangled in the SSR build of the app, which means that Models will have the wrong name and will wind up pointing at non-existent (or wrong) D1 tables
    • v3_lazyRouteDiscovery is a new default behavior for remix that doesn’t require any other changes to enable

--- templates/remix/vite.config.ts
+++ templates/remix/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     remix({
       future: {
         v3_fetcherPersist: true,
+        v3_lazyRouteDiscovery: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
       },
@@ -22,8 +23,5 @@ export default defineConfig({
   },
   resolve: {
     mainFields: ["browser", "module", "main"],
-  },
-  build: {
-    minify: true,
   },
 });
